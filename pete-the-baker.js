78612d39b6a2cc467618971724ba27ddf38fd14d
Jamie <Jamiekaram16@gmail.com>
2020-04-01 17:25:37 -0400

    --

--- pete-the-baker.js
+++ pete-the-baker.js
@@ -13,5 +13,7 @@ function cakes(recipe, available) {
     return Math.min(...calc)
 }
 
-console.log(cakes({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 })); //  return 2
-console.log(cakes({ apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }, { sugar: 500, flour: 2000, milk: 2000 })); // return 0
+
+
+// console.log(cakes({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 })); //  return 2
+// console.log(cakes({ apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }, { sugar: 500, flour: 2000, milk: 2000 })); // return 0
