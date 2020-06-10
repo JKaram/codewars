053479b52d78b469e5d486b61bac8ad1dfff777e
jkaram <jamiekaram16@gmail.com>
2020-06-09 22:27:34 -0400

    First Python problem!

--- smallestUnusedID.py
+++ smallestUnusedID.py
@@ -1,1 +1,21 @@
-print('hello')
+def next_id(arr):
+    if not arr:
+        return 0
+
+    arr.sort()
+    if arr[0] != 0:
+        return 0
+
+    for i in range(len(arr) - 1):
+        if arr[i] - arr[i + 1] < -1:
+            return arr[i] + 1
+    
+    return arr[-1] + 1
+
+
+y = [0,1,2,3,4,5,6,7,8,9,10] # 11 
+x = [0,0,0,0,0,0] # 1
+
+print(next_id(y))
+
+# and sort[num] - 1 != -1
